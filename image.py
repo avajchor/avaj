7985a429bb19060b7043315cda8cbda3243229d7
avajchor <avajchor@gmail.com>
2024-10-24 08:51:58 +0530

    Create image.py
    Signed-off-by: avajchor <avajchor@gmail.com>

---
+++ image.py
@@ -0,0 +1,9 @@
+import time
+from picamera import
+PiCamera
+camera-PiCamera()
+camera.resolution=(1280,7 20)
+camera.start_preview()
+time.sleep(5)
+camera.capture('home/pi/Desktop/ty2.jpg')
+camera.stop_preview()
